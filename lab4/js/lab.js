3cf9b73d9949d71cc372875e202060c1ba31b05e
auaguila <auaguila@ucsc.edu>
2024-10-17 22:27:02 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -9,6 +9,21 @@
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
+
+  // Snake
+  // An implementation of the game
+  // Aurora Aguilar <auaguilar@ucsc.edu>
+  // October 2024
+
+  //print a 17x15 checkerboard
+  //while snake == alive
+    //randomize x,y (spot on board)
+    //print apple
+    //if snake eats apple
+      //increse snake len by 1
+    //if snake runs into wall or lands on self
+      //snake == dead
+
   // return results;
 }
 

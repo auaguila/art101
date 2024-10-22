3277cc4cbfa615b0bbdf0c17c93fb146c2674d39
auaguila <auaguila@ucsc.edu>
2024-10-21 22:54:32 -0700

    lab 4

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,28 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Aurora Aguilar
+// Date: 10/12/2024
 
 // Constants
 
 // Functions
+
+//Variables
+make = "Chevrolet";
+model = "Impala";
+color = "Blue";
+year = 1964;
+ownIt = false;
+
+//calculation
+age = 2024 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years <br>");
+document.writeln("Own it: " + ownIt + "<br>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
